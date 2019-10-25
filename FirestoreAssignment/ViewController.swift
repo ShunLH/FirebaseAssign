43ae9dad21337648b101a51d2bdc08d242efc8ef
ShunLH <shunleihmu.shunlh@gmail.com>
2019-10-25 21:23:50 +0630

    Initial Commit

---
+++ FirestoreAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoreAssignment
+//
+//  Created by AcePlus Admin on 10/25/19.
+//  Copyright © 2019 SLH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
